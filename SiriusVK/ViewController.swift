f2f759d957b71e897538fa85c757b9475f68d708
Vladimir Louis <vovangus1235@gmail.com>
2022-07-12 21:57:27 +0300

    Initial Commit

---
+++ SiriusVK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SiriusVK
+//
+//  Created by Vladimir Gusev on 12.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
